f56d74147d08f885cc236c8007594c41011212b6
[peddollashashidhar] <[peddolla.shashidhar@gmail.com]>
2024-06-07 21:29:25 +0530

    commit

--- gunicorn_app/server.py
+++ gunicorn_app/server.py
@@ -1,7 +1,11 @@
 from gunicorn_app import app
-from flask import render_template
+from flask import Flask
 
 @app.route("/")
 def home():
     #return render_template('index.html')
     return "Hello World from Hasura"
+
+if __name__ == "__main__":
+	app.config['TEMPLATES_AUTO_RELOAD'] = True
+	app.run(host='0.0.0.0', debug=True, port=8080)
